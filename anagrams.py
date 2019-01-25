31dc2ab2be4c03e76a387ae95581d744b496a76a
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-01-25 20:13:52 +0100

    Create anagrams.py

---
+++ anagrams.py
@@ -0,0 +1,12 @@
+#https://www.codewars.com/kata/523a86aa4230ebb5420001e1/train/python
+
+def anagrams(word, words):
+    word_composition = { letter : word.count(letter) for letter in word}
+    anagrams_list = []
+    for _word in words:
+        _word_comp = { letter : _word.count(letter) for letter in _word}
+        if _word_comp == word_composition:
+            anagrams_list.append(_word)
+    return anagrams_list
+
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
