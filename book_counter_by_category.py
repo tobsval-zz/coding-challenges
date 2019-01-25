90c3ab31eeb272fd26e346597557f10400e361a2
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-01-25 21:54:40 +0100

    Minor correction on line 4

--- book_counter_by_category.py
+++ book_counter_by_category.py
@@ -1,7 +1,7 @@
 #https://www.codewars.com/kata/help-the-bookseller/train/python
 
 def stock_list(list_of_art, list_of_cat):
-    category_dict = {}.fromkeys(list_of_cat, 0)
+    category_dict = dict().fromkeys(list_of_cat, 0)
     for book in list_of_art:
         if book[0] in category_dict:
             category_dict[book[0]] += int(book.split(' ')[1])
