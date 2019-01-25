4cb5fc29e8a43664439548f445d12662a5de03ec
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-01-25 22:31:17 +0100

    Minor optimization

--- narcissistic_number.py
+++ narcissistic_number.py
@@ -1,7 +1,8 @@
 #https://www.codewars.com/kata/does-my-number-look-big-in-this/train/python
 
 def narcissistic(value):
-    digits = [int(dig) for dig in str(value)]
-    num = sum(digit ** len(str(value)) for digit in digits)
+    val = str(value)
+    digits = [int(dig) for dig in val]
+    num = sum(digit ** len(val) for digit in digits)
     return True if num == value else False
 
