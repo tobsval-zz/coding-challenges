ff83e36bc890e49f24752e6d0f98012374263149
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-01-26 21:41:11 +0100

    Create roman_numerals.py

---
+++ roman_numerals.py
@@ -0,0 +1,32 @@
+#https://www.codewars.com/kata/51b66044bce5799a7f000003/train/python
+
+class RomanNumerals:
+    def __init__(self, arg):
+        if isinstance(arg, int):
+            self.to_roman(arg)
+        elif isinstance(arg, str):
+            self.from_roman(arg)
+        return None
+
+    def to_roman(self, num : int):
+        alphabet, str_num = {1 : 'I', 5 : 'V', 10 : 'X', 50 : 'L', 100 : 'C', 500 : 'D', 1000 : 'M'}, str(num)
+        num_units = [int(value) for value in str_num]
+        num_units.sort()
+        roman_number = str()
+        for exp in range(len(str_num)):
+            num_units[exp] = num_units[exp] * (10 ** exp)
+            if num_units[exp] in alphabet:
+                roman_number += alphabet[num_units[exp]]
+        print(roman_number)
+
+    def from_roman(self, string : str):
+        alphabet = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        pass
+
+    def is_close_to_tens(number : int):
+        #If a number is 1 unit away from a multiple of 10 it needs a special roman representation
+        return True if number % 10 == 9 else False
+
+RomanNumerals(543)
+
+#Work in Progress
