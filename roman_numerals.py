0e609826a1e1b47929fd2e3b93fd0ab381829367
Tobia Valerio <tobiavalerio.studio@gmail.com>
2019-01-26 21:44:19 +0100

    Update roman_numerals.py

--- roman_numerals.py
+++ roman_numerals.py
@@ -10,9 +10,8 @@ class RomanNumerals:
 
     def to_roman(self, num : int):
         alphabet, str_num = {1 : 'I', 5 : 'V', 10 : 'X', 50 : 'L', 100 : 'C', 500 : 'D', 1000 : 'M'}, str(num)
-        num_units = [int(value) for value in str_num]
+        num_units, roman_number = [int(value) for value in str_num], str()
         num_units.sort()
-        roman_number = str()
         for exp in range(len(str_num)):
             num_units[exp] = num_units[exp] * (10 ** exp)
             if num_units[exp] in alphabet:
@@ -23,9 +22,12 @@ class RomanNumerals:
         alphabet = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
         pass
 
-    def is_close_to_tens(number : int):
+    def is_close_to_tens(self, number : int):
         #If a number is 1 unit away from a multiple of 10 it needs a special roman representation
         return True if number % 10 == 9 else False
+
+    def find_order_of_magnitude(self, number : int, magnitudes : list):
+        pass
 
 RomanNumerals(543)
 
